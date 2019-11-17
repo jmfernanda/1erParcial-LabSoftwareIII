756d34249f03f9f8370c158d6a9d4032b1b486a6
Fernanda Jaramillo <mariaft@unicauca.edu.co>
2019-11-17 16:04:22 -0500

    estilo actualizado

--- tareas.php
+++ tareas.php
@@ -9,29 +9,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="Estilos/style.css">
-    <title>Document</title>
+    <title>Tareas</title>
+
     <script src="https://code.jquery.com/jquery-3.4.1.js"  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
     crossorigin="anonymous"></script>
     <script src="gestionTareas.js"></script>
 
 </head>
 <body>
-
-    <h1 class="titulo1">¡Bienvenido!</h1>
-    <div id="informacion" class="centrado">
+    <nav class="barra">
+    <ul>
+        <li><p>Visor de tareas</p></li>
+        <li style="float:right"><p>Tu identificación: <?php echo $_POST['inputUsuario']?></p></li>
+        </ul>
+    </nav>
+    
+    <div  class="centrado">
+        <h1 id="titulo1">¡Bienvenido!</h1>
         <p>
-            Este es un nuevo visualizador de tareas. Aquí puedes llevar un control de todas tu tareas tachando
-            las ya realizadas o dejándolas sin tachar cuando están pendientes. Además,
-            puedes agregar nuevas tareas si así lo deseas.
+            Este es un nuevo visualizador de tareas. Aquí puedes llevar un control de todas las labores que tienes
+            que realizar. Puedes tachar las tareas cuando ya han sido realizadas o dejarlas sin tachar cuando aún
+            están pendientes. Además, puedes agregar nuevas tareas si lo necesitas.
         </p>
-        <br>
-        <label>
-            Tu número de identificación: <?php echo $_POST['inputUsuario']?>
-        </label>
     </div>
-    <br>
-    <h2 id="tituloLista" class="centrado">Tu lista de tareas</h2>
     <div id="container" class="centrado">
+       <h2 id="tituloLista">Tu lista de tareas</h2>
         <ul class="lista" id="lista">
             <?php
                 for($i=0; $i<count($json); $i++):?>
